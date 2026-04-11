3226e10b39a7c03580579f33ca111f1db1bf5cca
AKzPrisma <ahmadkurniaprisma@gmail.com>
2026-04-12 06:56:19 +0700

    chore: define main structure pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/assistant" element={<Assistant />} />
+        <Route path="/history" element={<History />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/auth/ResetPass.tsx
@@ -0,0 +1,7 @@
+export default function ResetPass(){
+    return(
+        <main>
+            
+        </main>
+    );
+}
