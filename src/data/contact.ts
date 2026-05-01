c88a6081f6fe5e0ce4d040090806281eb2045e52
AKzPrisma <ahmadkurniaprisma@gmail.com>
2026-05-01 22:41:20 +0700

    chore: add links contact

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -5,21 +5,21 @@ export const contact = [
     { 
         id: 1, 
         icon: HiOutlineMail, 
-        link: "#" },
+        link: "https://mail.google.com/mail/u/0/#inbox?compose=CllgCJZdBMnWpPJmLjDvNHxQmZNDLBrrqPwvxccTnkddvxzxrqBVBvBQjQsmFrFStdWKrwCFzNB" },
     { 
         id: 2, 
         icon: FaWhatsapp, 
-        link: "#" },
+        link: "https://wa.me/628989209565" },
     { 
         id: 3, 
         icon: FaInstagram, 
-        link: "#" },
+        link: "https://instagram.com/akprisma" },
     { 
         id: 4, 
         icon: FaTiktok, 
-        link: "#" },
+        link: "https://tiktok.com/@royuciha246" },
     { 
         id: 5, 
         icon: FaGithub, 
-        link: "#" }
+        link: "https://github.com/ak7prisma" }
 ];
